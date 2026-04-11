90050b47ab314f0e9e2d5204156ff7656cd9879f
Vansh Gosavi <vanshgosavi7@gmail.com>
2026-04-11 12:12:31 +0530

    Final

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.js',
-    env: {
-      VITE_API_URL: 'https://mock-api.example.com/Stage/',
-    },
-    coverage: {
-      provider: 'v8',
-      reporter: ['text', 'lcov'],
-      include: ['src/**/*.{js,jsx}'],
-      exclude: ['src/main.jsx', 'src/test/**'],
-    },
-  },
   server: {
     proxy: {
       '/api': {
